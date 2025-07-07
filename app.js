123ec11bb49f4056c7c3ab1d1d13b4301d1bc8c7
AKNPusat <aromakayunusantarpusat@gmail.com>
2025-07-08 02:24:25 +0700

    Update app.js

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function() {
             <div class="header-container">
                 <a href="index.html" class="logo-link">
                     <!-- GANTI DENGAN LINK LOGO PNG TRANSPARAN ANDA -->
-                    <img src="https://i.imgur.com/8Yv6ZcD.png" alt="Logo PT. Aroma Kayu Nusantara" class="logo">
+                    <img src="https://raw.githubusercontent.com/AKNPusat/AromaKayuNusantara/f1aefa4404a1950a09bcee398914efd334d62c95/logo%20AROMA%20kayu.png" class="logo">
                 </a>
                 <nav class="main-nav">
                     <!-- Menu "Tentang Kami" -->
@@ -55,6 +55,11 @@ document.addEventListener("DOMContentLoaded", function() {
                             <a href="firma.html">Firma AKN</a>
                             <a href="bmt.html">Bina Multi Transindo</a>
                         </div>
+                    </div>
+                    <!-- Menu "Keberlanjutan" -->
+                    <div class="nav-item">
+                         <a href="keberlanjutan.html" class="nav-link">Keberlanjutan</a>
+                    </div>
                     </div>
                     <!-- Menu "Keberlanjutan" -->
                     <div class="nav-item">
