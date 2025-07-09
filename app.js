7572e11fc33452ddc757ee79686d8ea8fb060d45
AKNPusat <aromakayunusantarpusat@gmail.com>
2025-07-10 02:34:32 +0700

    Update app.js

--- app.js
+++ app.js
@@ -26,52 +26,37 @@ document.addEventListener("DOMContentLoaded", function() {
                     <img src="https://raw.githubusercontent.com/AKNPusat/AromaKayuNusantara/f1aefa4404a1950a09bcee398914efd334d62c95/logo%20AROMA%20kayu.png" class="logo">
                 </a>
                 <nav class="main-nav">
-                    <!-- Menu "Tentang Kami" -->
-                    <div class="nav-item">
-                        <a href="#" class="nav-link">Tentang Kami</a>
-                        <div class="dropdown-menu">
-                        .dropdown-menu {
-                                <div class="dropdown-menu">
-                                <a href="tentang-kami.html#sekilas-akn">Sekilas AKN</a>
-                                <a href="tentang-kami.html#tonggak-sejarah">Tonggak Sejarah</a>
-                                <a href="tentang-kami.html#visi-misi">Visi, Misi, & Tata Nilai</a>
-                                <a href="tentang-kami.html#makna-logo">Makna Logo</a>
-                                <a href="tentang-kami.html#direksi">Manajemen</a>
-                        </div>
+                <div class="nav-item">
+                    <a href="#" class="nav-link">Tentang Kami</a>
+                    <div class="dropdown-menu">
+                        <a href="tentang-kami.html#sekilas-akn">Sekilas AKN</a>
+                        <a href="tentang-kami.html#tonggak-sejarah">Tonggak Sejarah</a>
                     </div>
-                    <!-- Menu "Bisnis Kami" -->
-                    <div class="nav-item">
-                        <a href="#" class="nav-link">Bisnis Kami</a>
-                        <div class="dropdown-menu">
-                            <a href="bisnis-akn1.html">AKN 1</a>
-                            <a href="bisnis-akn2.html">AKN 2</a>
-                            <a href="bisnis-akn3.html">AKN 3</a>
-                        </div>
+                </div>
+                <div class="nav-item">
+                    <a href="#" class="nav-link">Bisnis Kami</a>
+                    <div class="dropdown-menu">
+                        <a href="#">Layanan 1</a>
                     </div>
-                    <!-- Menu "Media & Informasi" -->
-                    <div class="nav-item">
-                        <a href="#" class="nav-link">Media & Informasi</a>
-                         <div class="dropdown-menu">
-                            <a href="newsroom.html">AKN News Room</a>
-                            <a href="publikasi.html">Publikasi</a>
-                            <a href="kegiatan.html">Kegiatan</a>
-                        </div>
+                </div>
+                <div class="nav-item">
+                    <a href="#" class="nav-link">Media & Informasi</a>
+                    <div class="dropdown-menu">
+                        <a href="#">Berita</a>
                     </div>
-                    <!-- Menu "Mitra AKN" -->
-                    <div class="nav-item">
-                        <a href="#" class="nav-link">Mitra AKN</a>
-                         <div class="dropdown-menu">
-                            <a href="firma.html">Firma AKN</a>
-                            <a href="bmt.html">Bina Multi Transindo</a>
-                        </div>
+                </div>
+                <div class="nav-item">
+                    <a href="#" class="nav-link">Mitra AKN</a>
+                    <div class="dropdown-menu">
+                        <a href="#">Mitra A</a>
                     </div>
-                    <!-- Menu "Keberlanjutan" -->
-                    <div class="nav-item">
-                         <a href="keberlanjutan.html" class="nav-link">Keberlanjutan</a>
-                        </div>       
-                        <div class="nav-item">
-                         <a href="lacak.html" class="nav-link">Lacak Resi</a>
-                    </div>
+                </div>
+                 <div class="nav-item">
+                    <a href="keberlanjutan.html" class="nav-link">Keberlanjutan</a>
+                </div>
+                <div class="nav-item">
+                    <a href="lacak.html" class="nav-link">Lacak Resi</a>
+                </div>
                 </nav>
                 <div class="header-search">
                      <svg fill="currentColor" viewBox="0 0 24 24" style="width:24px;height:24px;"><path d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" /></svg>
