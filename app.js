24310d5d214f65ea9b849620adac662ceb112697
AKNPusat <aromakayunusantarpusat@gmail.com>
2025-07-12 13:43:42 +0700

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,6 @@ document.addEventListener("DOMContentLoaded", function() {
                     <a href="lacak.html" class="nav-link">Lacak Resi</a>
                 </div>
                 </nav>
-                <div class="header-right">
                     <div class="header-search">
                      <svg fill="currentColor" viewBox="0 0 24 24" style="width:24px;height:24px;"><path d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z" /></svg>
                 </div>
@@ -175,14 +174,4 @@ document.addEventListener('DOMContentLoaded', function() {
     // Mulai autoplay
     startSlideShow();
 });
-// --- 4. Logika untuk Hamburger Menu Mobile ---
-const hamburger = document.querySelector('.hamburger-button');
-const mainNav = document.querySelector('.main-nav');
 
-if (hamburger && mainNav) {
-    hamburger.addEventListener('click', function() {
-        // Toggle class 'active' pada menu dan tombol hamburger
-        mainNav.classList.toggle('active');
-        hamburger.classList.toggle('active');
-    });
-}
