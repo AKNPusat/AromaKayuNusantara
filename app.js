14c950e9db2a1b9561cadc995b54fb51da1515e8
AKNPusat <aromakayunusantarpusat@gmail.com>
2025-07-25 01:22:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -75,6 +75,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
      const headerPlaceholder = document.getElementById('header-placeholder');
     if (headerPlaceholder) { headerPlaceholder.innerHTML = headerHTML; }
+    const hamburgerBtn = document.querySelector('.hamburger-button');
+        const mainNav = document.querySelector('.main-nav');
+
+        if (hamburgerBtn && mainNav) {
+            hamburgerBtn.addEventListener('click', () => {
+                mainNav.classList.toggle('nav-open');
+            });
+        }
+    }
     const footerPlaceholder = document.getElementById('footer-placeholder');
     if (footerPlaceholder) { footerPlaceholder.innerHTML = footerHTML; }
 
