2cf85cda9fbf79a21bc4de3cb802d139b12fa468
AKNPusat <aromakayunusantarpusat@gmail.com>
2025-07-25 04:23:27 +0700

    Update app.js

--- app.js
+++ app.js
@@ -160,3 +160,11 @@ document.addEventListener("DOMContentLoaded", function() {
     }
 });
 
+// --- Logika untuk Hamburger Menu ---
+const hamburger = document.querySelector('.hamburger-button');
+const mainNav = document.querySelector('.main-nav');
+if (hamburger && mainNav) {
+    hamburger.addEventListener('click', () => {
+        mainNav.classList.toggle('active');
+    });
+}
